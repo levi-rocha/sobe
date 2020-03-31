e908e607854397ecd8a57437daebe9f1d1f8c6ee
Levi Rocha <ilevidr@gmail.com>
2020-03-31 14:04:58 -0400

    scan virustotal fonctionne

--- Controllers/DownloadController.cs
+++ Controllers/DownloadController.cs
@@ -2,9 +2,13 @@ using System;
 using System.IO;
 using System.IO.Compression;
 using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using SOBE.Models;
 
 namespace SOBE.Controllers
@@ -13,6 +17,9 @@ namespace SOBE.Controllers
     [Route("[controller]")]
     public class DownloadController : ControllerBase
     {
+        private const string VIRUSTOTAL_APIKEY = "ec544f93a52305cafa0273d4d3ac54e89db0d94a82468b5a9130ef1bb1e9b7ba";
+        private const string ENDPOINT_VIRUSTOTAL_REPORT = "https://www.virustotal.com/vtapi/v2/file/report";
+        private const string ENDPOINT_VIRUSTOTAL_SCAN = "https://www.virustotal.com/vtapi/v2/file/scan";
         private IWebHostEnvironment _env;
         private string _storagePath;
 
@@ -60,10 +67,11 @@ namespace SOBE.Controllers
             var downloadDir = System.IO.Directory.CreateDirectory(AbsPath(requestId.ToString()));
             var result = new RequestHandle { Id = requestId, Ready = false };
 
-            Task.Run(async () => await ProcessFile(downloadRequest.FileUrl, downloadDir.FullName, outputName)).ContinueWith((r) => {
+            Task.Run(async () => await ProcessFile(downloadRequest.FileUrl, downloadDir.FullName, outputName)).ContinueWith((r) =>
+            {
                 if (r.IsCompletedSuccessfully)
                     Console.WriteLine($"[ProcessFile] [{outputName}] Success");
-                else 
+                else
                 {
                     foreach (var exception in r.Exception.InnerExceptions)
                     {
@@ -87,7 +95,7 @@ namespace SOBE.Controllers
             System.IO.Directory.CreateDirectory(srcPath);
             var downloadedPath = Path.Combine(srcPath, outputName);
             var zippedPath = Path.Combine(downloadDir, $"{Path.GetFileNameWithoutExtension(outputName)}.zip");
-            
+
             // Download
             using (var client = new HttpClient())
             {
@@ -101,12 +109,95 @@ namespace SOBE.Controllers
                 }
             }
 
+            // Scan
+            string sha1;
+            using (FileStream fs = new FileStream(downloadedPath, FileMode.Open))
+            using (BufferedStream bs = new BufferedStream(fs))
+            {
+                using (SHA1Managed sha1managed = new SHA1Managed())
+                {
+                    byte[] hash = sha1managed.ComputeHash(bs);
+                    StringBuilder formatted = new StringBuilder(2 * hash.Length);
+                    foreach (byte b in hash)
+                    {
+                        formatted.AppendFormat("{0:X2}", b);
+                    }
+                    sha1 = formatted.ToString();
+                }
+            }
+
+            await Scan(sha1, downloadedPath);
+
             // Zip
             ZipFile.CreateFromDirectory(Path.Combine(downloadDir, "src"), zippedPath);
 
             // Cleanup
             System.IO.Directory.Delete(srcPath, recursive: true);
             await System.IO.File.WriteAllTextAsync(Path.Combine(downloadDir, "work.done"), zippedPath);
+        }
+
+        private static async Task Scan(string sha1, string filePath, int maxAttempts = 10)
+        {
+            bool uploaded = false;
+            bool isSafe;
+            int attempts = 0;
+            do
+            {
+                attempts++;
+                if (TryScan(sha1, out isSafe))
+                {
+                    if (!isSafe)
+                        throw new Exception("Detected as virus by VirusTotal");
+                    else
+                        return;
+                }
+                else
+                {
+                    if (!uploaded)
+                    {
+                        await UploadForScan(filePath);
+                        uploaded = true;
+                    }
+                    // wait for scan
+                    Thread.Sleep(10000);
+                }
+            } while (attempts < maxAttempts);
+        }
+
+        private static async Task UploadForScan(string filePath)
+        {
+            var content = new MultipartFormDataContent();
+            content.Add(new StringContent(VIRUSTOTAL_APIKEY), "apikey");
+            var fileBytes = await System.IO.File.ReadAllBytesAsync(filePath);
+            content.Add(new ByteArrayContent((fileBytes), 0, fileBytes.Length), "file");
+            using (var client = new HttpClient())
+            {
+                var response = await client.PostAsync(ENDPOINT_VIRUSTOTAL_SCAN, content);
+                if (!response.IsSuccessStatusCode)
+                    throw new Exception($"Could not upload to VirusTotal: {response.Content.ReadAsStringAsync().Result}");
+            }
+        }
+
+        private static bool TryScan(string sha1, out bool isSafe)
+        {
+            try
+            { isSafe = IsSafe(sha1); return true; }
+            catch
+            { isSafe = false; return false; }
+        }
+
+        private static bool IsSafe(string sha1)
+        {
+            dynamic response;
+            using (var client = new HttpClient())
+            {
+                var urlReport = $"{ENDPOINT_VIRUSTOTAL_REPORT}?resource={sha1}&apikey={VIRUSTOTAL_APIKEY}";
+                response = JsonConvert.DeserializeObject<dynamic>(client.GetAsync(urlReport).Result.Content.ReadAsStringAsync().Result);
+            }
+            if (response.response_code == 1)
+                return response.positives == 0;
+            else
+                throw new Exception($"VirusTotal error! Response code: {response.response_code} | Message: {response.verbose_msg}");
         }
 
         public string AbsPath(string fileName) => Path.Combine(_storagePath, fileName);
